b4d1de2875f07cb60a44af2dbcaa972b5fa286be
Ryan Mileris <ryanmileris@yahoo.com>
2020-03-16 23:08:31 -0600

    moved files

---
+++ Run Markov/DataClasses/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataClasses
+{
+    public class State
+    {
+        public int StateNum { get; private set; }
+        public double InitialValue { get; private set; }
+
+        public State(int stateNum, double initialValue)
+        {
+            StateNum = stateNum;
+            InitialValue = initialValue;
+        }
+
+
+    }
+}
